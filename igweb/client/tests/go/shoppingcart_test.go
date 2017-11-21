c3decd3a52b0f4124b1eead845b13e5e5af9688d
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-11-21 04:58:50 -0800

    Added delay before verification of the correct product item and product quantity in shopping cart.

--- igweb/client/tests/go/shoppingcart_test.go
+++ igweb/client/tests/go/shoppingcart_test.go
@@ -37,7 +37,7 @@ func main() {
 	})
 
 	// Verify product added and quantity
-	casper.Call("then", func() {
+	casper.Call("wait", 450, func() {
 		casper.Get("test").Call("assertTextExists", "Swiss Army Knife", "Display correct product in shopping cart.")
 		casper.Get("test").Call("assertTextExists", "Quantity: 3", "Display correct product quantity in shopping cart.")
 	})
